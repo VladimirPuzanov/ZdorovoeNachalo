5918a2abaafe1116d4e9a91b29f45ae77b26fe00
Vladimir Puzanov <volodya938@gmail.com>
2024-07-16 17:19:52 +0300

    Начал верстать главную страницу, сверстал стартовый экран, сделал блок о клинике, направления делаятельности, этапы оказания услуги, начал верстать прайслист

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+const pricelistSwiperCheck = document.querySelectorAll('pricelist');
+if (pricelistSwiperCheck.length > 0) {
+	pricelistSwiperCheck.forEach((slider) => {
+		const pricelistSlider = new Swiper(slider.querySelector('pricelist__swiper'), {
+			direction: 'horizontal',
+			slidesPerView: 1.1,
+			grabCursor: true,
+			spaceBetween: 10,
+			navigation: {
+				nextEl: slider.querySelector('nextButton'),
+				prevEl: slider.querySelector('prevButton'),
+			},
+			breakpoints: {
+				360: {
+					slidesPerView: 1,
+					spaceBetween: 20,
+				},
+				950: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				}
+			}
+		});
+	})
+}
