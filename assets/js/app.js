e29f87331f160eff1696a9aa64047cd47005d010
VladimirPuzanov <volodya938@yandex.ru>
2024-07-21 23:24:47 +0300

    Добавил ещё ряд js скриптов, добавил функционал попапов

--- assets/js/app.js
+++ assets/js/app.js
@@ -156,3 +156,106 @@ document.querySelectorAll('a[href^="#"').forEach(link => {
 		});
 	});
 });
+
+/* Mask phone */
+[].forEach.call(document.querySelectorAll('input[type=tel]'), function (input) {
+	let keyCode;
+	function mask(event) {
+		event.keyCode && (keyCode = event.keyCode);
+		let pos = this.selectionStart;
+		if (pos < 3) event.preventDefault();
+		let matrix = "+7 (___) ___ ____",
+			i = 0,
+			def = matrix.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, ""),
+			new_value = matrix.replace(/[_\d]/g, function (a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+			});
+		i = new_value.indexOf("_");
+		if (i != -1) {
+			i < 5 && (i = 3);
+			new_value = new_value.slice(0, i)
+		}
+		let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+			function (a) {
+				return "\\d{1," + a.length + "}"
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+		if (event.type == "blur" && this.value.length < 5) this.value = ""
+	}
+
+	input.addEventListener("input", mask, false);
+	input.addEventListener("focus", mask, false);
+	input.addEventListener("blur", mask, false);
+	input.addEventListener("keydown", mask, false)
+
+});
+/* End Mask phone */
+
+// Popups
+function popupClose(popupActive) {
+	popupActive.classList.remove('open');
+	document.body.classList.remove('lock');
+	document.querySelector('html').removeAttribute('style');
+	document.querySelector('html').classList.remove('lock');
+
+}
+const popupOpenBtns = document.querySelectorAll('.popup-btn');
+const popups = document.querySelectorAll('.popup');
+const originalTitlePopup2 = document.querySelector('.original-title').innerHTML;
+const closePopupBtns = document.querySelectorAll('.close-popup');
+closePopupBtns.forEach(function (el) {
+	el.addEventListener('click', function (e) {
+		popupClose(e.target.closest('.popup'));
+	});
+});
+popupOpenBtns.forEach(function (el) {
+	el.addEventListener('click', function (e) {
+		e.preventDefault();
+		const path = e.currentTarget.dataset.path;
+		const currentPopup = document.querySelector(`[data-target="${path}"]`);
+		if (currentPopup) {
+			popups.forEach(function (popup) {
+				popupClose(popup);
+				popup.addEventListener('click', function (e) {
+					if (!e.target.closest('.popup__content')) {
+						popupClose(e.target.closest('.popup'));
+					}
+				});
+			});
+			currentPopup.classList.add('open');
+			if (currentPopup.getAttribute('data-target') == 'popup-change') {
+				let originaTitle = currentPopup.querySelector('.original-title');
+				if (el.classList.contains('change__item-btn')) {
+					if (el.classList.contains('change__item-btn_current')) {
+						originaTitle.textContent = el.textContent;
+					}
+					else {
+						let currentItem = el.closest('.change__item-title');
+						let currentTitile = currentItem.querySelector('.current-title');
+						if (el.classList.contains('doctor__btn')) {
+							originaTitle.innerHTML = 'Записаться на приём к врачу:' + currentTitile.innerHTML
+						}
+						else {
+							originaTitle.innerHTML = currentTitile.innerHTML
+						}
+					}
+				}
+
+				else {
+					originaTitle.innerHTML = originalTitlePopup2;
+				}
+			}
+			if (el.classList.contains('reviews__btn')) {
+
+				let currentItem = el.closest('.reviews__item')
+				let originalName = currentPopup.querySelector('.reviews__name_original');
+				let originalText = currentPopup.querySelector('.reviews__text_original');
+				originalName.innerHTML = currentItem.querySelector('.reviews__name').innerHTML;
+				originalText.innerHTML = currentItem.querySelector('.reviews__text').innerHTML;
+			}
+		}
+	});
+});
+/* end popups */
